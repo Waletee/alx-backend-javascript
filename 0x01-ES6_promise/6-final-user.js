a2b52aa1ff3e12f8a229077f40148fb0267ac04a
Taiwo Abiodun <taiwobiodun@gmail.com>
2023-06-27 18:02:20 +0200

    hot fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,16 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+/* eslint-disable import/named */
+// eslint-disable-next-line import/named
+import { signUpUser } from "./4-user-promise";
+import { uploadPhoto } from "./5-photo-reject";
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises).then((res) =>
-    res.map((res) => ({
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) =>
+    result.map((res) => ({
       status: res.status,
-      value: res.status === "fulfilled" ? res.value : `${res.reason}`,
+      value: res.status === "fulfilled" ? res.value : res.reason,
     }))
   );
 }
-
-export default handleProfileSignup;
