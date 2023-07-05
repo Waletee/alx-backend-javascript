bba0ed24f662d3a26d1ad653dc15b56ba1780b20
Taiwo Abiodun <taiwobiodun@gmail.com>
2023-07-05 12:38:33 +0200

    copy configuration files and fix task errors

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,22 +2,22 @@ export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Dennis',
-  lastName: 'Ritchie',
+  firstName: "Wale",
+  lastName: "Adekunle",
   experienceTeachingC: 10,
 };
 
-console.log('C++');
+console.log("C++");
 cpp.setTeacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-console.log('Java');
+console.log("Java");
 java.setTeacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-console.log('React');
+console.log("React");
 react.setTeacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
